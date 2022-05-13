326ea80457ea10181ef6cdd0dfd5cfba8c5bb790
jakubfurmanowicz <furmanowiczjakub@gmail.com>
2022-05-13 09:18:36 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -58,76 +45,83 @@ class _MyHomePageState extends State<MyHomePage> {
             decoration: BoxDecoration(gradient: GetGradientByGroup(_group)),
             child: Center(
                 child: SingleChildScrollView(
-              child: Column(children: [
-                Padding(padding: EdgeInsets.only(top: 20)),
-                Text(
-                  'Podaj wagę w kg:',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-                Container(
-                  width: 150,
-                  height: 25,
-                  child: TextField(
-                    controller: _weightOfBodyController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 5)),
-                Text('Podaj wzrost w cm:',
-                    style: TextStyle(fontSize: 18, color: Colors.white)),
-                Container(
-                  width: 150,
-                  height: 25,
-                  child: TextField(
-                    controller: _heightOfBodyController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 10)),
-                ElevatedButton(
-                    onPressed: CalculateYourBmi,
-                    style: ElevatedButton.styleFrom(primary: Colors.blue),
-                    child:
+                  child: Column(children: [
+                    Padding(padding: EdgeInsets.only(top: 20)),
+                    Text(
+                      'Podaj wagę w kg:',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    Padding(padding: EdgeInsets.only(bottom: 5)),
+                    Container(
+                      width: 150,
+                      height: 25,
+                      child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          controller: _weightOfBodyController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white54)
+                              ),
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.only(
+                                  left: 5, top: 0, bottom: 0, right: 3))
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 5)),
+                    Text('Podaj wzrost w cm:',
+                        style: TextStyle(fontSize: 18, color: Colors.white)),
+                    Padding(padding: EdgeInsets.only(bottom: 3)),
+                    Container(
+                      width: 150,
+                      height: 25,
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        controller: _heightOfBodyController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white54)
+                            ),
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.only(
+                                left: 5, top: 0, bottom: 0, right: 3)),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    ElevatedButton(
+                        onPressed: CalculateYourBmi,
+                        style: ElevatedButton.styleFrom(primary: Colors.blue),
+                        child:
                         const Text('Oblicz', style: TextStyle(fontSize: 18))),
-                Padding(padding: EdgeInsets.only(top: 10)),
-                Text(
-                  'BMI wynosi: ${_result}${_group}',
-                  style: TextStyle(fontSize: 20, color: Colors.indigo),
-                ),
-                Text(
-                  '$_group',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-                Padding(padding: EdgeInsets.only(top: 10)),
-                Text('$_error')
-              ]),
-            ))));
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    Text(
+                      'BMI wynosi: ${_result}',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    Text(
+                      'Twoja grupa: ${_group}',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    Text('$_error',
+                        style: TextStyle(fontSize: 20, color: Colors.white))
+                  ]),
+                ))));
   }
 
   LinearGradient? GetGradientByGroup(String _group) {
-    if (_group == 'wyglodzenie' || _group == 'wychudzenie') {
+    if (_group == 'wychudzenie' || _group == 'niedowaga' || _group == 'wygłodzenie') {
       return LinearGradient(
           begin: Alignment.bottomCenter,
           end: Alignment.topCenter,
-          colors: [new Color(0xff3519c2), new Color(0xff4c7ef2)]);
-    } else if (_group == 'niedowaga') {
-      return LinearGradient(
-          begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
-          colors: [new Color(0xff1552dd), new Color(0xff4cb4f2)]);
+          colors: [new Color(0xff1b1893), new Color(0xff4aa9ed)]);
     } else if (_group == 'waga prawidłowa') {
       return LinearGradient(
           begin: Alignment.bottomCenter,
           end: Alignment.topCenter,
-          colors: [new Color(0xff21F32BFF), new Color(0xff8BF56DFF)]);
-    } else if (_group == 'nadwaga') {
-      return LinearGradient(
-          begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
-          colors: [new Color(0xffAD0202FF), new Color(0xffff0000)]);
-    } else if (_group == 'otyłość I stopnia' ||
+          colors: [new Color(0xff1c9e19), new Color(0xff7df24c)]);
+    } else if (_group == 'nadwaga' ||_group == 'otyłość I stopnia' ||
         _group == 'otyłość II stopnia' ||
         _group == 'otyłość III stopnia') {
       return LinearGradient(
@@ -137,10 +131,11 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void CalculateYourBmi(){
-    String error = 'Wprowadź poprawne wartości';
+  void CalculateYourBmi() {
+    String error = 'Wprowadź poprawne wartości!';
 
-    if (!_heightOfBodyController.text.isEmpty) {
+    if (!_heightOfBodyController.text.isEmpty ||
+        !_weightOfBodyController.text.isEmpty) {
       double weight = double.parse(_weightOfBodyController.text);
       double hight = double.parse(_heightOfBodyController.text) / 100;
       double result = weight / (hight * hight);
